7d43f22e3d5e6dd68d5d874087d3d919154f0aa8
miladesmailpour <milad.esmailpour@gmail.com>
2023-08-01 16:40:49 -0400

    add the post method to server.js

--- text-analyze/server.js
+++ text-analyze/server.js
@@ -15,4 +15,20 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.post("/text-analyze", async (req, res) => {
+  const { prompt } = req.body;
 
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    max_tokens: 512,
+    temperature: 0,
+    prompt: prompt,
+  });
+  res.send(completion.data.choices[0].text);
+});
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
