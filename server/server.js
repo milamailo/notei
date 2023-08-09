24ed8366dc901a0604040251dd442429cf6139be
miladesmailpour <milad.esmailpour@gmail.com>
2023-08-09 17:44:09 -0400

    config the server for  production

--- server/server.js
+++ server/server.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const { ApollpServer } = require("apollo-server-express");
 const path = require("path");
-
 const { resolvers, typeDefs } = require("./schemas");
+const db = require("./config/connection");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApollpServer({
+  resolvers,
+  typeDefs,
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
 
