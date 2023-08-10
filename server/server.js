30329f6acf9f8d6f7b1cc5aba112c4b26fcf7158
miladesmailpour <milad.esmailpour@gmail.com>
2023-08-09 22:32:17 -0400

    add the instance of apollo server and start it

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { ApollpServer } = require("apollo-server-express");
+const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
 const { resolvers, typeDefs } = require("./schemas");
 const db = require("./config/connection");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const server = new ApollpServer({
+const server = new ApolloServer({
   resolvers,
   typeDefs,
 });
@@ -17,3 +17,22 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+const apolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
+};
+
+apolloServer.start();
