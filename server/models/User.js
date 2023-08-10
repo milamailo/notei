98d9ca21b1f8a2561d5cd70f42a2bf5b1fdb6301
miladesmailpour <milad.esmailpour@gmail.com>
2023-08-10 15:59:00 -0400

    erfacto the user model to make username from email

--- server/models/User.js
+++ server/models/User.js
@@ -53,24 +53,35 @@ const userSchema = new Schema({
   },
 });
 
-// Combining pre-hooks into a single "save" middleware
-userSchema.pre("save", async function (next) {
-  if (!this.username) {
-    this.username = this.email;
+UserSchema.pre("save", async function (next) {
+  // Pre-save hook to fill the userName field from the email
+  if (!this.userName) {
+    this.userName = this.email.split("@")[0].toLowerCase();
+    this.email = this.email.toLowerCase();
+    this.firstName = this.firstName.toLowerCase();
+    if (this.lastName) {
+      this.lastName = this.lastName.toLowerCase();
+    } else {
+      this.lastName = "";
+    }
   }
-
+  // set up pre-save middleware to create password
   if (this.isNew || this.isModified("password")) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
-
   next();
 });
 
-userSchema.methods.isCorrectPassword = async function (password) {
+// Apply unique validator plugin
+UserSchema.plugin(uniqueValidator);
+
+// // compare the incoming password with the hashed password
+UserSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-const User = model("User", userSchema);
+// User model
+const User = model("User", UserSchema);
 
 module.exports = User;
