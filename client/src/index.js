c39159e911b415ac63c01fc50ca198701dd06f53
miladesmailpour <milad.esmailpour@gmail.com>
2023-08-14 18:42:43 -0400

    front-end is running

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <h1>hi from Mics-Note</h1>
+    </div>
   </React.StrictMode>,
   document.getElementById("root")
 );
