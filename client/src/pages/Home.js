9f99c148e57597c99395c43f550119bd428d7a06
miladesmailpour <milad.esmailpour@gmail.com>
2023-08-14 22:18:57 -0400

    setup the router in App.js

---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <main>
+      <div className="flex-row justify-center">
+        <h1>hi from Home.js!</h1>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
