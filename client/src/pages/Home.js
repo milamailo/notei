a8d826665869c9936b31c096d8a449d490956277
miladesmailpour <milad.esmailpour@gmail.com>
2023-08-14 22:43:06 -0400

    tested the all user query in Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_ALL_USERS } from "../utils/queries";
 
 const Home = () => {
+  const { loading, data } = useQuery(QUERY_ALL_USERS);
+  const users = data?.users || [];
+
   return (
     <main>
       <div className="flex-row justify-center">
-        <h1>hi from Home.js!</h1>
+        <div className="col-12 col-md-8 mb-3">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ul>
+              {users.map((user) => (
+                <li key={user._id}>
+                  <p>
+                    User: {user.firstname} {user.email}
+                  </p>
+                  {/* Render other user information here */}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
     </main>
   );
