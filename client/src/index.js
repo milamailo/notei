3bd78386c1ce623fb43d6955478fc487bff3ce16
miladesmailpour <milad.esmailpour@gmail.com>
2023-08-14 20:49:55 -0400

    App.js added to handel routing between pages

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from "./App";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>
-      <h1>hi from Mics-Note</h1>
-    </div>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
