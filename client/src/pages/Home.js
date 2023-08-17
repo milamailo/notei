cca747bee256c488e20c9389fae5ebd30d7df49b
miladesmailpour <milad.esmailpour@gmail.com>
2023-08-17 16:00:36 -0400

    moveed the intro component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 import { QUERY_ALL_USERS } from "../utils/queries";
 import Auth from "../utils/auth";
-import Intro from "./components/Intro";
+import Intro from "../components/";
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_ALL_USERS);
@@ -12,29 +12,6 @@ const Home = () => {
     <main>
       <div className="flex-row justify-center">
         <div className="col-12 col-md-8 mb-3">
-          {/* {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <ul>
-              {Auth.loggedIn() ? (
-                users.map((user) => (
-                  <li key={user._id}>
-                    <p>User: {user.username}</p>
-                    <ul>
-                      {user.notes.map((note) => (
-                        <li key={note._id}>
-                          <p>User: {note.title}</p>
-                        </li>
-                      ))}
-                    </ul>
-                  </li>
-                ))
-              ) : (
-                <h1>Empty</h1>
-              )}
-            </ul>
-          )} */}
-
           {!Auth.loggedIn() ? (
             <Intro />
           ) : loading ? (
