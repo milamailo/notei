16ece44d819dcdb33511bcb61892b6b9469d199d
miladesmailpour <milad.esmailpour@gmail.com>
2023-08-16 21:24:43 -0400

    add the intro component to display in home before login

---
+++ client/src/pages/components/Intro.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <>
+      <p>
+        Taking notes is a valuable practice for several reasons, and it offers
+        several advantages that can aid in learning, comprehension, and memory
+        retention. Here are some of the key benefits of taking notes:
+      </p>
+      <ul>
+        <li>
+          <h3>Active Engagement:</h3> Taking notes involves active processing
+          and summarization of information.
+        </li>
+        <li>
+          <h3>Enhanced Focus:</h3> Note-taking helps maintain focus on important
+          points and ideas.
+        </li>
+        <li>
+          <h3>Organization:</h3> Notes provide a structured format to organize
+          information logically.
+        </li>
+        <li>
+          <h3>Memory Enhancement:</h3> Writing down information aids in memory
+          retention and recall.
+        </li>
+        <li>
+          <h3>Summarization:</h3> Note-taking encourages distillation of complex
+          ideas into concise summaries.
+        </li>
+        <li>
+          <h3>Personalization:</h3> Notes reflect your understanding, making
+          information more relatable.
+        </li>
+        <li>
+          <h3>Review and Repetition:</h3> Notes are useful for review,
+          reinforcing learning over time.
+        </li>
+        <li>
+          <h3>Preparation for Assessments:</h3> Well-organized notes serve as
+          study guides for exams.
+        </li>
+        <li>
+          <h3>Capture Visual Aids:</h3> Notes can include diagrams and visuals
+          to aid understanding.
+        </li>
+        <li>
+          <h3>Active Listening:</h3> Note-taking promotes attentive listening
+          during lectures.
+        </li>
+        <li>
+          <h3>Reflective Learning:</h3> Reviewing notes helps identify gaps and
+          encourages deeper understanding.
+        </li>
+        <li>
+          <h3>Communication Practice:</h3> Note-taking skills translate into
+          effective communication.
+        </li>
+        <li>
+          <h3>Reduces Cognitive Load:</h3> Notes store information, reducing the
+          need for memorization.
+        </li>
+        <li>
+          <h3>Long-Term Reference:</h3> Notes serve as resources for revisiting
+          concepts over time.
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Intro;
