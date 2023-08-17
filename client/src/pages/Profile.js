0b7e2d81898f5b32d52ef2cdc38acd531a74c938
miladesmailpour <milad.esmailpour@gmail.com>
2023-08-17 16:42:31 -0400

    created profile and test it

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import { Navigate, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import React from "react";
+import { Navigate, useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
 
+import { QUERY_USER, QUERY_ME } from "../utils/queries";
 
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
-
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 
 const Profile = () => {
+  console.log("Profile -> ");
   const { username: userParam } = useParams();
-
+  console.log("Profile -userParam -> ", userParam);
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam },
   });
 
-  const user = data?.me || data?.user || {};
+  const user = data?.authUser || data?.userByEmailOrUserName || {};
+  console.log("Profile -user -> ", user);
   // navigate to personal profile page if username is yours
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
     return <Navigate to="/me" />;
@@ -37,23 +38,18 @@ const Profile = () => {
     <div>
       <div className="flex-row justify-center mb-3">
         <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+          Viewing {userParam ? `${user.username}'s` : "your"} profile.
         </h2>
 
         <div className="col-12 col-md-10 mb-5">
-          <ThoughtList
-            thoughts={user.thoughts}
-            title={`${user.username}'s thoughts...`}
-            showTitle={false}
-            showUsername={false}
-          />
+          <h5>Note List</h5>
         </div>
         {!userParam && (
           <div
             className="col-12 col-md-10 mb-3 p-3"
-            style={{ border: '1px dotted #1a1a1a' }}
+            style={{ border: "1px dotted #1a1a1a" }}
           >
-            <ThoughtForm />
+            <h5>Note Form</h5>
           </div>
         )}
       </div>
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -3,21 +3,47 @@ const jwt = require("jsonwebtoken");
 const secret = "how know, who know";
 const expiration = "1h";
 
+// module.exports = {
+//   authMiddleware: function ({ req }) {
+//     const token = req.headers.authorization || "";
+
+//     if (!token) {
+//       return {};
+//     }
+
+//     try {
+//       const { data } = jwt.verify(token, secret, { maxAge: expiration });
+//       return { user: data }; // Set the user data in the context
+//     } catch (error) {
+//       console.log("Invalid token", error.message);
+//       return {}; // Return an empty context if token is invalid
+//     }
+//   },
+//   signToken: function ({ email, username, _id }) {
+//     const payload = { email, username, _id };
+//     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+//   },
+// };
 module.exports = {
   authMiddleware: function ({ req }) {
-    const token = req.headers.authorization || "";
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
+    }
 
     if (!token) {
-      return {};
+      return req;
     }
 
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      return { user: data }; // Set the user data in the context
-    } catch (error) {
-      console.log("Invalid token", error.message);
-      return {}; // Return an empty context if token is invalid
+      req.user = data;
+    } catch {
+      console.log("Invalid token");
     }
+
+    return req;
   },
   signToken: function ({ email, username, _id }) {
     const payload = { email, username, _id };
