ba411bf70715a63ba07202f355dc0588e5131f29
miladesmailpour <milad.esmailpour@gmail.com>
2023-08-22 16:28:04 -0400

    modify dicphone and add style and structre

--- client/src/components/Dictaphone/index.js
+++ client/src/components/Dictaphone/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { createSpeechlySpeechRecognition } from '@speechly/speech-recognition-polyfill';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import React from "react";
+import { createSpeechlySpeechRecognition } from "@speechly/speech-recognition-polyfill";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 // appId MUST move to the .env file before repo being available to public
-const appId = 'fdc5337d-095d-42be-b1b0-11b8833365f1';
+const appId = "fdc5337d-095d-42be-b1b0-11b8833365f1";
 const SpeechlySpeechRecognition = createSpeechlySpeechRecognition(appId);
 SpeechRecognition.applyPolyfill(SpeechlySpeechRecognition);
 
@@ -12,9 +14,10 @@ const Dictaphone = () => {
     transcript,
     listening,
     resetTranscript,
-    browserSupportsSpeechRecognition
+    browserSupportsSpeechRecognition,
   } = useSpeechRecognition();
-  const startListening = () => SpeechRecognition.startListening({ continuous: true });
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true });
 
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
@@ -22,15 +25,40 @@ const Dictaphone = () => {
 
   return (
     <div>
-      <p>Microphone: {listening ? 'on' : 'off'}</p>
-      <button
-        onTouchStart={startListening}
-        onMouseDown={startListening}
-        onTouchEnd={SpeechRecognition.stopListening}
-        onMouseUp={SpeechRecognition.stopListening}
-      >Hold to talk</button>
-      <button onClick={resetTranscript}>Reset</button>
-      <p>{transcript}</p>
+      <div className="d-flex flex-row bg-light">
+        <div>
+          <h5 className="card-header bg-primary text-light p-2 m-1">
+            Microphone: {listening ? "on" : "off"}
+          </h5>
+        </div>
+        <div className="ml-auto ">
+          <button className="bg-primary text-light p-2 m-1">keep it!</button>
+          <button className="bg-primary text-light p-2 m-1">keep it!</button>
+          <button className="bg-primary text-light p-2 m-1">keep it!</button>
+          <button
+            className="bg-primary text-light p-2 m-1"
+            onClick={resetTranscript}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
+      <div className="d-flex flex-row bg-primary justify-content-center">
+        <div className="p-2">
+          <p className="text-light">{transcript}</p>
+        </div>
+        <div className="p-2">
+          <button
+            className="bg-primary text-light p-2 m-0 offset-md-3"
+            onTouchStart={startListening}
+            onMouseDown={startListening}
+            onTouchEnd={SpeechRecognition.stopListening}
+            onMouseUp={SpeechRecognition.stopListening}
+          >
+            Note it!
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
