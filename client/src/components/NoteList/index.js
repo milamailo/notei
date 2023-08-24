3cc3f0b92c25a915ebda30211497bf2b8389a506
miladesmailpour <milad.esmailpour@gmail.com>
2023-08-24 01:53:33 -0400

    order the note list based on creaded tile

--- client/src/components/NoteList/index.js
+++ client/src/components/NoteList/index.js
@@ -2,43 +2,47 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const NoteList = ({ notes, title, showTitle = true, showUsername = true }) => {
-  if (!notes.length) {
+  // Sort notes by createdAt in descending order
+  const sortedNotes = notes
+    .slice()
+    .sort((a, b) => b.createdAt.localeCompare(a.createdAt));
+
+  if (!sortedNotes.length) {
     return <h3>No Note Yet</h3>;
   }
 
   return (
     <div>
       {showTitle && <h3>{title}</h3>}
-      {notes &&
-        notes.map((note) => (
-          <div key={note._id} className="card mb-3">
-            <h4 className="card-header bg-primary text-light p-2 m-0">
-              {showUsername ? (
-                <Link className="text-light" to={`/profiles/${note}`}>
-                  {note.title} <br />
-                  <span style={{ fontSize: "1rem" }}>
-                    had this note on {note.createdAt}
-                  </span>
-                </Link>
-              ) : (
-                <>
-                  <span style={{ fontSize: "1rem" }}>
-                    You had this note on {note.createdAt}
-                  </span>
-                </>
-              )}
-            </h4>
-            <div className="card-body bg-light p-2">
-              <p>{note.text}</p>
-            </div>
-            <Link
-              className="btn btn-primary btn-block btn-squared"
-              to={`/notes/${note._id}`}
-            >
-              add Subnote to this note.
-            </Link>
+      {sortedNotes.map((note) => (
+        <div key={note._id} className="card mb-3">
+          <h4 className="card-header bg-primary text-light p-2 m-0">
+            {showUsername ? (
+              <Link className="text-light" to={`/profiles/${note}`}>
+                {note.title} <br />
+                <span style={{ fontSize: "1rem" }}>
+                  had this note on {note.createdAt}
+                </span>
+              </Link>
+            ) : (
+              <>
+                <span style={{ fontSize: "1rem" }}>
+                  You had this note on {note.createdAt}
+                </span>
+              </>
+            )}
+          </h4>
+          <div className="card-body bg-light p-2">
+            <p>{note.text}</p>
           </div>
-        ))}
+          <Link
+            className="btn btn-primary btn-block btn-squared"
+            to={`/notes/${note._id}`}
+          >
+            add Subnote to this note.
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
